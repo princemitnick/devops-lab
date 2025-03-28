19fd57f1b703f9d1832f6a555e717b164c17c250
Prince stanley Jean baptiste <prstanley@Princes-MacBook-Air.local>
2025-03-28 06:23:45 -0400

    lab 1 - exploring webapp

---
+++ jour-1/lab-1.sh
@@ -0,0 +1,31 @@
+az login 
+
+
+#Creation d'un groupe de ressources
+
+az group create --name FirstGroup --location eastus
+
+
+#Creation app service
+
+az appservice plan create --name AppServicePlan --resource-group FirstGroup --sku FREE
+
+#Deploiement d'une web app
+
+az webapp create --resource-group $RG --plan AppServicePlan --name MyApp --runtime "node|14-lts"
+
+
+#Verification webApp
+
+az webapp show --resource-group $RG --name MyApp
+
+
+#Supprimer un App Service Plan 
+
+az appservice plan delete --name AppServicePlan --resource-group $RG --yes
+
+
+#lister environnement disponible
+
+az webapp list-runtimes --os-type linux
+
